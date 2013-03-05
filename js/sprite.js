d4db232f8dc7d4e92fd99ecf3b3729fabd6df205
James Long <jlongster@jlongster.com>
2013-03-05 18:14:27 -0500

    maor cleanup

--- js/sprite.js
+++ js/sprite.js
@@ -16,39 +16,40 @@
             this._index += this.speed*dt;
         },
 
-        render: function(ctx) {
-            var frame;
+render: function(ctx) {
+    var frame;
 
-            if(this.speed > 0) {
-                var max = this.speed === 0 ? 1 : this.frames.length;
-                var idx = Math.floor(this._index);
-                frame = this.frames[idx % max];
-            }
-            else {
-                frame = 0;
-            }
+    if(this.speed > 0) {
+        var max = this.frames.length;
+        var idx = Math.floor(this._index);
+        frame = this.frames[idx % max];
 
-            if(this.once && idx >= max) {
-                this.done = true;
-                return;
-            }
-
-            var x = this.pos[0];
-            var y = this.pos[1];
-
-            if(this.dir == 'vertical') {
-                y += frame * this.size[1];
-            }
-            else {
-                x += frame * this.size[0];
-            }
-
-            ctx.drawImage(resources.get(this.url),
-                          x, y,
-                          this.size[0], this.size[1],
-                          0, 0,
-                          this.size[0], this.size[1]);
+        if(this.once && idx >= max) {
+            this.done = true;
+            return;
         }
+    }
+    else {
+        frame = 0;
+    }
+
+
+    var x = this.pos[0];
+    var y = this.pos[1];
+
+    if(this.dir == 'vertical') {
+        y += frame * this.size[1];
+    }
+    else {
+        x += frame * this.size[0];
+    }
+
+    ctx.drawImage(resources.get(this.url),
+                  x, y,
+                  this.size[0], this.size[1],
+                  0, 0,
+                  this.size[0], this.size[1]);
+}
     };
 
     window.Sprite = Sprite;
